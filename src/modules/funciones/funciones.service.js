f80e6d510eff42861ff89c5b12199c2f8572514e
SneatX <santi02005@hotmail.com>
2024-07-31 01:38:44 -0500

    feat: :sparkles: use case 5 finished

--- src/modules/funciones/funciones.service.js
+++ src/modules/funciones/funciones.service.js
@@ -5,6 +5,30 @@ export async function verDisponibilidadAsientos(idFuncion){
 
     //Obtener asientos ocupados y limites
     let funcion = await funcionesCollection.getFuncionById(idFuncion)
-    if(funcion) return { resultado: "error", mensaje: "Tarjeta ya registrado anteriormente" }
-    let limite = await funcionesCollection.getAsientosFuncion(idFuncion)
+    if(!funcion) return { resultado: "error", mensaje: "Id de la funcion invalida" }
+
+    let asientosData = await funcionesCollection.getAsientosFuncion(idFuncion)
+
+    const alfabeto = "abcdefghijklmnopqrstuvwxyz"
+
+    let asientosDisponibles = []
+    for(let letra of alfabeto.toUpperCase()){
+        if(letra <= asientosData.limitFila){
+            let contador = 1
+            while (contador <= parseInt(asientosData.limitCol)){
+                let newLetra = `${letra}${contador}`
+                if(!asientosData.asientosOcupados.includes(newLetra)){
+                    asientosDisponibles.push(newLetra)
+                }
+                contador++
+            }
+        }
+    }
+
+    return {
+        asientos_libres: asientosDisponibles,
+        mensaje: "Array ordenado de asientos libres  para la funcion"
+    }
+
+    
 }

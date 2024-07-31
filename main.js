44fcc60287a7cb3fdedafd7a79000f055e678ae7
SneatX <santi02005@hotmail.com>
2024-07-31 01:05:55 -0500

    feat: :sparkles: Case use 5
    feat: :construction: First validation use case 5
    4e222db51afdad2b4b2bd7753fa11c80845aa8b0
    
    feat: :sparkles: use case 5 finished
    f80e6d510eff42861ff89c5b12199c2f8572514e

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { casoUso1 } from "./src/modules/clientes/clientes.controller.js";
 import { casoUso2, casoUso3 } from "./src/modules/peliculas/peliculas.controller.js";
 import { casoUso4 } from "./src/modules/boletas/boletas.controller.js";
+import { casoUso5 } from "./src/modules/funciones/funciones.controller.js";
 
 // casoUso1()
 // casoUso2()
 // casoUso3()
-casoUso4()
+// casoUso4()
+casoUso5()

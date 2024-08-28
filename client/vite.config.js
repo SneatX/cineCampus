f3d379bb604f2b3fc0c43bb7858d231bc1cf75ce
SneatX <santi02005@hotmail.com>
2024-08-28 11:00:43 -0500

    feat: :zap: new vite configuration to use the same port always

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
+
+// Cargar el archivo .env
+dotenv.config({ path: '../.env' })
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = process.env
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST, 
+      port: Number(env.VITE_PORT) 
+    }
+  }
 })

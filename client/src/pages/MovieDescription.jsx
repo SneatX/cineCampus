55c56d4ba80c5bc97d688b537a4631475e3c7e5b
SneatX <santi02005@hotmail.com>
2024-08-28 19:19:03 -0500

    feat: :sparkles: New returnHeader created

--- client/src/pages/MovieDescription.jsx
+++ client/src/pages/MovieDescription.jsx
@@ -1,11 +1,20 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { ReturnHeader } from "../components/ReturnHeader"
+
+import '../css/pages/MovieDescription.css'
 
 export function MovieDescription(){
 
     const { idPeli } = useParams()
+    const navigate = useNavigate()
+    const returnFunction = () =>{
+        navigate('/menu')
+    }
 
     return (
-        <ReturnHeader />
+        <main className="movieDescription-container">
+            <ReturnHeader title='Cinema Section' returnFunction={returnFunction}/>
+
+        </main>
     )
 }
